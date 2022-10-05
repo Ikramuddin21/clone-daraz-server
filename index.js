18499978877d729752d598a70a91abf24f6ca4ef
ikramuddin21 <ikramuddin2122@gmail.com>
2022-10-05 11:51:37 +0600

    Server connection and get all product

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilfiw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try {
+        await client.connect();
+        const database = client.db('online_shop');
+        const productsCollection = database.collection('products');
+
+        app.get('/products', async (req, res) => {
+           const cursor = productsCollection.find({});
+           const result = await cursor.toArray();
+           res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Clone Daraz server running');
+});
+
+app.listen(port, () => {
+    console.log('Clone daraz app listening port', port);
+});
