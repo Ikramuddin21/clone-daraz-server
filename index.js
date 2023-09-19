878ab95bf8c73c529bfc3d6000981a7f88b72d3e
Ikramuddin21 <shaonakhand2122@gmail.com>
2023-09-19 16:26:23 +0600

    get products details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,33 +9,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilfiw.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run () {
-    try {
-        await client.connect();
-        const database = client.db('online_shop');
-        const productsCollection = database.collection('products');
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("online_shop");
+    const productsCollection = database.collection("products");
 
-        app.get('/products', async (req, res) => {
-           const cursor = productsCollection.find({});
-           const result = await cursor.toArray();
-           res.json(result);
-        })
-    }
-    finally {
-        // await client.close();
-    }
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    app.get("/productDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.json(product);
+    });
+  } finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Clone Daraz server running');
+app.get("/", (req, res) => {
+  res.send("Clone Daraz server running");
 });
 
 app.listen(port, () => {
-    console.log('Clone daraz app listening port', port);
+  console.log("Clone daraz app listening port", port);
 });
